e507dd4b19b98f2b827a071891f30d98ef86a9b9
Ribulose <okposiokparobor@gmail.com>
2025-05-09 00:07:28 +0100

    modifcation landingpage layout

--- src/app/landingpage/layout.tsx
+++ src/app/landingpage/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import SiderBar from "../DashboardComp/SiderBar";
 import Navbar from "../DashboardComp/Navbar";
 
 const geistSans = Geist({
