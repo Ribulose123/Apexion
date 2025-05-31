177a0c3e483b476ac44e1b52c2692a13be7a8d1a
Ribulose <okposiokparobor@gmail.com>
2025-05-31 01:08:40 +0100

    reset password

--- src/app/(dash)/settings/newpassword/page.tsx
+++ src/app/(dash)/settings/newpassword/page.tsx
@@ -1,8 +1,12 @@
 import NewPassword1 from "@/app/components/NewPassword"
+import { Suspense } from "react"
 export default function NewPassword(){
     return(
         <div className="flex-col p-2 sm:p-4 mt-14">
-            <NewPassword1/>
+            
+            <Suspense fallback={<div>Loading...</div>}>
+                <NewPassword1/>
+        </Suspense>
         </div>
     )
 }
