37d0c56406929f0b4924af0532ce7e90035a2d0a
Ribulose <okposiokparobor@gmail.com>
2025-05-31 01:01:12 +0100

    email verfication page modfication

--- src/app/(auth)/emailverfi/page.tsx
+++ src/app/(auth)/emailverfi/page.tsx
@@ -1,9 +1,14 @@
+import { Suspense } from "react";
 import EmailVerification from "@/app/Auth content/EmailVerification";
+
 
 export default function EmailVerify() {
    
 
     return(
-        <EmailVerification />
+        <Suspense fallback={<div>Loading...</div>}>
+                <EmailVerification />
+        </Suspense>
+        
     )
 }
